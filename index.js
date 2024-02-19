6c91102cbe846e2d932413e52ac10915e29f97c1
difenual <156965049+difenual@users.noreply.github.com>
2024-02-19 15:40:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// Carga el SDK de Twitter 
+window.twttr = (function(d, s, id) {
+
+  // Inserta el SDK asincronamente
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+
+  if (d.getElementById(id)) return t;
+
+  js = d.createElement(s); 
+  js.id = id;
+  js.async = true;
+  js.src = "https://platform.twitter.com/widgets.js";
+  
+  fjs.parentNode.insertBefore(js, fjs);
+   
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+  
+  return t;
+
+}(document, "script", "twitter-wjs"));
+
+// Cuando el DOM esta listo, renderiza
+document.addEventListener('DOMContentLoaded', function(){
+
+  // Renderiza el timeline de tweets
+  twttr.widgets.createTimeline({
+    sourceType: 'https://twitter.com/SCPI_',
+    screenName: 'SCPI_',
+    chrome: "noheader nofooter noborders noscrollbar transparent",
+  }, 
+  
+  document.getElementById('tweets-container'));
+
+});
