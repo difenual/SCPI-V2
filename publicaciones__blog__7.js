97405d61d5dcbe837d8faf25e089f6df41fa9155
difenual <156965049+difenual@users.noreply.github.com>
2024-03-12 09:56:38 -0500

    Add files via upload
    Publicación - Blog 7

---
+++ publicaciones__blog__7.js
@@ -0,0 +1,113 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Simula obtener datos de un servidor o base de datos
+    var publicaciones = [
+        
+        {
+            titulo: 'La Transparencia: Desafíos y Oportunidades en la Construcción Social (Diego Nuñez 12/03/2024)',
+            contenido: 'En un contexto latinoamericano considero la transparencia un pilar fundamental para garantizar la integridad no sólo de las instituciones estatales sino también de garantizar interacciones relacionales auténticas. Por esa razón, analizo este valor con la intención de atribuirle su importancia dentro de los esquemas éticos para que se implemente tanto en procesos de contratación pública como en el diario vivir.<br>La transparencia, acorde a la Real Academia Española, se define como la "Cualidad de transparente" y transparente significa "Claro, evidente, que se comprende sin duda ni ambigüedad". En este sentido, la transparencia implica claridad, apertura y ausencia de ocultamiento o ambigüedad. (RAE, 2024)<br>En el ámbito gubernamental, la transparencia se refiere a la obligación de los funcionarios públicos de enseñar información relevante sobre su gestión, decisiones y uso de recursos públicos. Según Transparencia Internacional, organización que promueve la transparencia a nivel global, "la transparencia es un principio que permite que todos los interesados conozcan los procesos de toma de decisiones, las responsabilidades de los actores, la información que sustenta las decisiones y los mecanismos de supervisión correspondientes" (Transparencia Internacional, 2022).<br>En el ámbito empresarial, la transparencia implica la divulgación de información relevante sobre las operaciones, estructura, situación financiera y prácticas de una empresa, con el fin de generar credibilidad en los inversionistas, clientes y otras partes interesadas. La transparencia corporativa es clave para mantener la sostenibilidad de las empresas, ya que permite tomar decisiones basadas en información verídica y crear confianza derivada de los procesos organizacionales.<br>En términos sociales, la transparencia se refiere a la posibilidad de relacionarnos de manera genuina, abierta y honesta con otros. Este valor nos permite expresar intereses, sensibilidades, pensamientos y crear una relación sincera con alguien más. Al aceptar nuestras intensidades emocionales, podemos ser abiertos a mostrar esas pulsiones no como un defecto sino como una sensación humana, una parte más de lo que somos para poder construir un vínculo profundo. Es una forma de evitar usar máscaras, fortalecer la posibilidad de mostrarnos lo más transparente posible y, por lo tanto, demostrar nuestras verdaderas intenciones.<br>En estos sentidos, a pesar de su reconocida importancia, la transparencia enfrenta diversos desafíos al implementarse efectivamente. A continuación, señalaré algunas de las principales razones que limitan su uso cotidiano en orden de reconciliarnos con la posibilidad de usarla como parte fundamental del desarrollo social:<br>- <b>Confidencialidad:</b> En ocasiones, la transparencia entra en conflicto con la necesidad de mantener información confidencial por razones de seguridad, privacidad o intereses estratégicos. Soy consciente que, al ser parte de una sociedad capitalista, integramos un mercado exigente y la información confidencial significa ventajas competitivas para sobrevivirlo. Sin embargo, es necesario encontrar un adecuado equilibrio con lo estrictamente secreto para encontrar herramientas de cooperación como una estrategia más interesante en el funcionamiento del mundo empresarial o gubernamental.<br>- <b>Accesibilidad de la información:</b> La simple divulgación de información no garantiza la transparencia si esta no es accesible, comprensible y relevante para los interesados. Por esta razón, es preciso encontrar formas intuitivas, dinámicas y pedagógicas para publicarla. En la medida que aumentemos  el nivel de comprensión lectora, será más útil la promoción de estos conocimientos. Un ejemplo es la plataforma digital para realizar los procesos de contratación pública del Estado llamada SECOP II. Aunque todavía hace falta un diseño óptimo es un esfuerzo estatal por democratizar la información de los procesos de compras nacionales.<br>- <b>Rendición de cuentas:</b> La transparencia por sí sola no es suficiente si no está acompañada de mecanismos efectivos de rendición de cuentas y sanciones por incumplimiento. Programar divulgaciones públicas para democratizar los resultados de proyectos permite involucrar a la ciudadanía y aplica una mayor presión a los procesos internos de las organizaciones. Esta exposición obliga a consolidar de manera más rigurosa las actividades y los medios requeridos para llegar a los resultados presentados. Es una forma de crear un ambiente auditor más integral.<br>- <b>Calidad de la información:</b> La información divulgada puede ser incompleta, sesgada o poco confiable, lo que socava la verdadera transparencia. Por eso, es fundamental crear hábitos de buenas referenciaciones, citaciones y demás recursos teóricos para asegurarnos de las fuentes bibliográficas. La calidad de los análisis posteriores proviene directamente del uso apropiado de manejar recursos teóricos lo más verídicos posibles. En este sentido, construimos criterios informados con la información más clara posible.<br>- <b>Cultura organizacional:</b> La falta de una cultura de honestidad dentro de las organizaciones dificulta la adopción justificada de prácticas transparentes. Parte de nuestras problemáticas al proyectar la transparencia en las organizaciones sociales, proviene del disgusto personal en usar la transparencia como parte fundamental en la creación de círculos afectivos. Los valores modernos como la exaltación de la superficialidad, el deseo de alcanzar el éxito económico, el elogio a la hipercompetitividad y la búsqueda maquiavélica por crear amistades con un interés particular atentan contra la posibilidad de vivir más armónicamente al aceptar nuestras diferencias, mostrando primero cómo somos. El menosprecio por no interiorizar la transparencia porque expone situaciones de vulnerabilidad en nuestras prácticas cotidianas es una de las razones por las cuales dejamos de usarlo en otras instancias democráticas. Al transformar nuestra relación inmediata con este valor, ampliaremos su implementación en otros espacios porque lo consideramos una parte importante de la realidad',
+            imagen: 'blog__7.jpg'
+        }
+    ];
+
+
+    
+
+
+
+    var listaPublicaciones = document.getElementById('lista-publicaciones');
+
+    // Iterar sobre las publicaciones y crear elementos HTML para cada una
+    publicaciones.forEach(function (publicacion, index) {
+        var article = document.createElement('article');
+        var titulo = document.createElement('h3');
+        var contenido = document.createElement('p');
+        var imagen = document.createElement('img');
+        var shareDiv = document.createElement('div');
+        var facebookBtn = document.createElement('a');
+        var twitterBtn = document.createElement('a');
+        var linkedinBtn = document.createElement('a');
+        var compartirTitulo = document.createElement('h4');
+        
+                
+        titulo.textContent = publicacion.titulo;
+        contenido.innerHTML = publicacion.contenido;
+        imagen.src = publicacion.imagen;
+
+        imagen.classList.add('publicaciones__imagenes');
+             
+        // Agregar clases y atributos necesarios para los botones de compartir
+        
+        facebookBtn.classList.add('share-button', 'share-facebook');
+        facebookBtn.innerHTML = '<i class="fab fa-facebook-f"></i>';
+
+        twitterBtn.classList.add('share-button', 'share-twitter');
+        twitterBtn.innerHTML = '<i class="fa-brands fa-x-twitter"></i>';
+        
+        linkedinBtn.classList.add('share-button', 'share-linkedin');
+        linkedinBtn.innerHTML = '<i class="fab fa-linkedin-in"></i>';
+
+        // Agregar botones de compartir al contenedor div
+        shareDiv.appendChild(facebookBtn);
+        shareDiv.appendChild(twitterBtn);
+        shareDiv.appendChild(linkedinBtn);
+
+        compartirTitulo.textContent = '¡Si te gustó este artículo puedes compartirlo en tus redes sociales!';
+
+              
+        // Agregar contenido y botones al artículo
+        article.appendChild(titulo);
+        article.appendChild(contenido);
+        article.appendChild(imagen);
+        article.appendChild(compartirTitulo);
+        article.appendChild(shareDiv);
+
+        // Agregar clase para aplicar estilos específicos
+        article.classList.add('blog-post');
+
+        // Agregar identificador único para cada publicación
+        article.id = 'post-' + index;
+
+        // Agregar artículo a la lista de publicaciones
+        listaPublicaciones.appendChild(article);
+    });
+
+    // Obtener todos los botones de compartir
+    var shareButtons = document.querySelectorAll('.share-button');
+
+    // Iterar sobre cada botón y agregar un evento de clic
+    shareButtons.forEach(function (button) {
+        button.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            // Obtener la URL de la página actual
+            var url = window.location.href;
+
+            // Obtener el título de la publicación actual
+            var titulo = button.parentNode.parentNode.querySelector('h3').textContent;
+
+            // Obtener la red social correspondiente al botón
+            var socialNetwork = button.classList.contains('share-facebook') ? 'facebook' :
+                                 button.classList.contains('share-twitter') ? 'twitter' :
+                                 button.classList.contains('share-linkedin') ? 'linkedin' : '';
+
+            // Generar el enlace de compartir según la red social
+            var shareUrl = '';
+            switch (socialNetwork) {
+                case 'facebook':
+                    shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url);
+                    break;
+                case 'twitter':
+                    shareUrl = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(titulo);
+                    break;
+                case 'linkedin':
+                    shareUrl = 'https://www.linkedin.com/shareArticle?url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(titulo);
+                    break;
+                default:
+                    break;
+            }
+
+            // Abrir una ventana emergente para compartir
+            window.open(shareUrl, '_blank', 'width=600,height=400');
+        });
+    });
+});
