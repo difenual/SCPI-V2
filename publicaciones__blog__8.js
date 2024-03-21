4f563269861833b1ef05214ca26e53fe475a280f
difenual <156965049+difenual@users.noreply.github.com>
2024-03-21 15:06:27 -0500

    Add files via upload
    Blog 8

---
+++ publicaciones__blog__8.js
@@ -0,0 +1,113 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Simula obtener datos de un servidor o base de datos
+    var publicaciones = [
+        
+        {
+            titulo: 'Reglas creadas en los Documentos Tipo aplicables a los proponentes plurales que deberían implementarse como buena práctica contractual (Sara Nuñez 21/03/2024)',
+            contenido: 'La implementación de los documentos tipo en el ordenamiento jurídico colombiano tiene como propósito fomentar la mayor participación de los proponentes en los procesos de contratación e incluir buenas prácticas contractuales. Así las cosas, en este texto explicaré cuatro (4) reglas creadas en los documentos tipo aplicables a los proponentes plurales para acreditar la experiencia mínima habilitante, conforme a las cuales se garantiza la idoneidad para ejecutar el proyecto y se incentiva la asociación de personas naturales o jurídicas para cumplir los requisitos del pliego.<br>La experiencia como requisito habilitante debe ser entendida como el conocimiento del proponente derivado de su participación previa en actividades iguales o similares a las previstas en el objeto del contrato, con contratos públicos, privados, nacionales o extranjeros(1). En el marco de la contratación pública, la experiencia del proponente se reviste de particular importancia, en la medida en que garantiza, en cierto grado, que no habrá improvisación ni mayores costos por errores o dificultades originadas en realizar una actividad por primera vez. Las cuatro (4) reglas creadas para la acreditación de la experiencia por parte del proponente plural son:<br>-	<b>Experiencia requerida</b>: En el caso de los proponentes plurales, en los documentos tipo para los proyectos de obra pública de infraestructura de transporte, agua potable y saneamiento básico e infraestructura social se exige lo siguiente: i) uno de los integrantes debe aportar como mínimo el cincuenta por ciento (50%) de la experiencia solicitada; ii) los demás integrantes deben acreditar al menos el cinco por ciento (5 %) de la experiencia solicitada; y iii) sin perjuicio de lo anterior, solo uno (1) de los integrantes, si así lo considera pertinente, podrá no acreditar experiencia. En este último caso, el porcentaje de participación del integrante que no aporta experiencia en la estructura plural no podrá superar el diez por ciento (10 %)(2).<br>De lo anterior, se demuestra que el documento tipo adoptado por la ANCP-CCE busca garantizar una idoneidad mínima para la ejecución del proyecto, debido a que la mayoría de los integrantes deben aportar experiencia -ya sea el 50% o mínimo el 5%-. De esta manera, el documento tipo permite que uno de los integrantes del proponente plural no acredite experiencia, si su porcentaje de participación no supera el diez por ciento (10%).<br>Sin duda, la exigencia de la experiencia prevista por Colombia Compra Eficiente logra dos propósitos: i) exigir una experiencia mínima habilitante que garantiza que la mayoría de los integrantes tengan la idoneidad para ejecutar el proyecto y ii) permitir que uno de los integrantes del proponente plural pueda participar en el proceso de contratación, aun cuando previamente no ha ejecutado contratos con objeto similar, y por tanto, este sea la primera oportunidad para adquirir experiencia con una Entidad pública en el proyecto a ejecutar.<br>-	<b>Experiencia aportada por un proponente plural en donde dos o más de sus integrantes han participado en proyectos anteriores y quieren presentarse al proceso de contratación:</b>  En los documentos tipo de proyectos de obra pública de infraestructura de transporte, agua potable y saneamiento básico e infraestructura social se exige que cuando el contrato aportado para acreditar la experiencia es ejecutado por un consorcio o unión temporal y dos (2) o más de sus integrantes conforman el proponente plural que participan en el proceso de contratación, dicho contrato se entenderá aportado como un (1) solo contrato y se tendrá en cuenta para el aporte de la experiencia la sumatoria de los porcentajes de participación de los integrantes del consorcio o unión temporal que ejecutaron el contrato y que están interviniendo en el proceso(3).<br>La regla creada por la ANCP-CCE evita que las Entidades validen una misma experiencia como si se hubiera ejecutado en dos oportunidades distintas, cuando en realidad se ejecutó en un mismo momento. Esta regla creada en el documento tipo aclara la forma cómo el proponente acredita la experiencia cuando integrantes del proponente plural, que pretenden participar en el proceso de contratación, se han asociado en otros proyectos previamente y quieren hacer valer esa experiencia en el proceso.<br>Por esta razón, la regla creada en el documento tipo precisa que solo se acreditará la experiencia una sola vez independiente del número de integrantes que se presente al proceso, y tomando como referencia el porcentaje de participación.<br>-	<b>Aportar mayores contratos para validar la experiencia cuando acredite la condición de MiPymes o emprendimiento y/o empresas de mujeres:</b> Con ocasión a la expedición de la Ley 2069 de 2020, el Congreso de la República crea criterios diferenciales en favor de las MiPymes, los emprendimientos y/o empresas de mujeres. Consecuentemente, prevé la posibilidad de acreditar la experiencia con un mayor número de contratos cuando el proponente es una MiPyme, un emprendimiento y/o empresas de mujeres(4).<br>En los documentos tipo para proyectos de infraestructura de transporte, agua potable y saneamiento básico e infraestructura social, los proponentes que acrediten alguna de las condiciones de MiPymes o emprendimiento y/o empresas de mujeres podrá acreditar la experiencia con un contrato adicional, salvo que acredite ambas condiciones que podrá acreditar dos (2) contratos adicionales(5).<br>Ahora bien, para el caso de los proponentes plurales bastará con que uno de sus integrantes tenga una participación igual o superior al diez por ciento (10 %) en el Consorcio o en la Unión Temporal y acredite la calidad de Mipyme o emprendimiento y empresa de mujer para aportar la mayor cantidad de contratos. Esta regla incentiva la constitución de proponentes plurales, puesto que otorga la posibilidad de aportar un mayor número de contratos para acreditar la experiencia a pesar de que solo uno (1) de los miembros del Consorcio o Unión Temporal cumple con la condición. Esta regla fue previamente definida en el parágrafo 1 del artículo 2.2.1.2.4.2.15. del Decreto 1860 de 2021.<br>-	<b>Acreditación de la experiencia por personas jurídicas con menos de tres (3) años de constituidas y se transfiere la experiencia de los accionistas, socios o constituyentes:</b> La Entidad tendrá en cuenta la experiencia de los accionistas, socios o constituyentes de las sociedades con menos de tres (3) años de estar constituidas. Pasado este tiempo, la sociedad conservará esta experiencia, tal y como haya quedado registrada en el RUP(6).<br>En los documentos tipo de obra pública de infraestructura de transporte, agua potable y saneamiento básico e infraestructura social se establece que para los casos en que se presente un Proponente Plural conformado por una persona jurídica, en conjunto con sus socios, accionistas o constituyentes y se alleguen contratos en los que estos le hayan transferido experiencia a aquella, solo podrán ser acreditados como experiencia en el Proceso de Contratación por uno de los integrantes, de manera tal que al Proponente Plural solo podrá avalársele una misma experiencia una sola vez(7).<br>Esto significa que, los documentos tipo aclaran que en el supuesto que en un mismo proceso de contratación se presente un proponente plural cuyo integrante sea una persona jurídica y los socios de esa persona jurídica, y éstos últimos le transfirieron la experiencia a la sociedad, solo podrá tenerse como experiencia la aportada por los socios. Sin duda, esta regla creada permite solventar una situación que, en la Ley no quedó definido de manera expresa, pero que en la práctica fue objeto de discusión si era factible validar una misma experiencia dos veces en el mismo proceso, en este caso tanto la presentada por la persona jurídica como la que sus socios le transfirieron cuando ambos son integrantes del proponente plural.<br><b>Conclusión</b><br>En este texto presenté cuatro (4) reglas creadas en los documentos tipo de obra pública de infraestructura de transporte, agua potable y saneamiento básico e infraestructura para acreditar la experiencia mínima habilitante por parte de los proponentes plurales.  Si bien estas reglas únicamente son obligatorias en los procesos de contratación condicionados a implementar los documentos tipo de obra pública, se recomienda a las Entidades Estatales adoptarlas en los procesos de contratación como una buena práctica contractual.<br>Lo anterior, debido a que estas reglas definen la forma más idónea para acreditar la experiencia por parte de los proponentes plurales, pero, además incentiva la constitución de Consorcios o Uniones Temporales para la presentación de ofertas en los procesos de contratación, sin que se afecte la adecuada ejecución del proyecto, así:<br>i)	Garantiza una idoneidad mínima por parte de todos los integrantes del proponente plural para ejecutar el proyecto -debido a que solicita que la mayoría de los miembros acrediten una experiencia mínima del 5% al 50%-, pero, además, permite que uno de los integrantes del proponente plural consiga su primera experiencia en la ejecución de un proyecto con una Entidad Pública, sin poner en riesgo el aval cumplimiento del proyecto.<br>ii)	Evita que una misma experiencia ejecutada por un proponente plural sea acreditada como si fueran dos contratos distintos. Así, se establece que la Entidad Estatal debe avalar la experiencia de acuerdo con la sumatoria del porcentaje de participación de los integrantes del proponente plural.<br>iii)	Replica la regla definida en el parágrafo 1 del artículo 2.2.1.2.4.2.15 del Decreto 1860 de 2021 en la forma cómo los proponentes deben acreditar la condición de MiPymes, los emprendimientos y/o empresas de mujeres, para ser beneficiarios de los criterios diferenciales. Sin duda, al exigir que basta con que uno de los integrantes del proponente plural con un porcentaje de participación de menos del 10% acredite la condición de MiPymes y/o emprendimiento o empresa de mujeres se incentiva la conformación de proponente plural.<br>iv)	Suple un vacío de la ley al definir que en el supuesto que un proponente plural cuyos miembros sea una persona jurídica que tiene menos de tres (3) años de constituida y los socios que le transfiere la experiencia a esta última. Así las cosas, la ANCP-CCE precisa que para el proponente plural solo podrá avalársele una misma experiencia una sola vez. Consecuentemente, se prohíbe que una misma experiencia se cuente como si fuera ejecutado en dos momentos diferentes.',
+            imagen: 'blog__8.jpg'
+        }
+    ];
+
+
+    
+
+
+
+    var listaPublicaciones = document.getElementById('lista-publicaciones');
+
+    // Iterar sobre las publicaciones y crear elementos HTML para cada una
+    publicaciones.forEach(function (publicacion, index) {
+        var article = document.createElement('article');
+        var titulo = document.createElement('h3');
+        var contenido = document.createElement('p');
+        var imagen = document.createElement('img');
+        var shareDiv = document.createElement('div');
+        var facebookBtn = document.createElement('a');
+        var twitterBtn = document.createElement('a');
+        var linkedinBtn = document.createElement('a');
+        var compartirTitulo = document.createElement('h4');
+        
+                
+        titulo.textContent = publicacion.titulo;
+        contenido.innerHTML = publicacion.contenido;
+        imagen.src = publicacion.imagen;
+
+        imagen.classList.add('publicaciones__imagenes');
+             
+        // Agregar clases y atributos necesarios para los botones de compartir
+        
+        facebookBtn.classList.add('share-button', 'share-facebook');
+        facebookBtn.innerHTML = '<i class="fab fa-facebook-f"></i>';
+
+        twitterBtn.classList.add('share-button', 'share-twitter');
+        twitterBtn.innerHTML = '<i class="fa-brands fa-x-twitter"></i>';
+        
+        linkedinBtn.classList.add('share-button', 'share-linkedin');
+        linkedinBtn.innerHTML = '<i class="fab fa-linkedin-in"></i>';
+
+        // Agregar botones de compartir al contenedor div
+        shareDiv.appendChild(facebookBtn);
+        shareDiv.appendChild(twitterBtn);
+        shareDiv.appendChild(linkedinBtn);
+
+        compartirTitulo.textContent = '¡Si te gustó este artículo puedes compartirlo en tus redes sociales!';
+
+              
+        // Agregar contenido y botones al artículo
+        article.appendChild(titulo);
+        article.appendChild(contenido);
+        article.appendChild(imagen);
+        article.appendChild(compartirTitulo);
+        article.appendChild(shareDiv);
+
+        // Agregar clase para aplicar estilos específicos
+        article.classList.add('blog-post');
+
+        // Agregar identificador único para cada publicación
+        article.id = 'post-' + index;
+
+        // Agregar artículo a la lista de publicaciones
+        listaPublicaciones.appendChild(article);
+    });
+
+    // Obtener todos los botones de compartir
+    var shareButtons = document.querySelectorAll('.share-button');
+
+    // Iterar sobre cada botón y agregar un evento de clic
+    shareButtons.forEach(function (button) {
+        button.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            // Obtener la URL de la página actual
+            var url = window.location.href;
+
+            // Obtener el título de la publicación actual
+            var titulo = button.parentNode.parentNode.querySelector('h3').textContent;
+
+            // Obtener la red social correspondiente al botón
+            var socialNetwork = button.classList.contains('share-facebook') ? 'facebook' :
+                                 button.classList.contains('share-twitter') ? 'twitter' :
+                                 button.classList.contains('share-linkedin') ? 'linkedin' : '';
+
+            // Generar el enlace de compartir según la red social
+            var shareUrl = '';
+            switch (socialNetwork) {
+                case 'facebook':
+                    shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url);
+                    break;
+                case 'twitter':
+                    shareUrl = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(titulo);
+                    break;
+                case 'linkedin':
+                    shareUrl = 'https://www.linkedin.com/shareArticle?url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(titulo);
+                    break;
+                default:
+                    break;
+            }
+
+            // Abrir una ventana emergente para compartir
+            window.open(shareUrl, '_blank', 'width=600,height=400');
+        });
+    });
+});
